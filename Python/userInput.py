ce01ccfe31ee898b104f2e7a711997f59e75361c
vyasparthm <vyasparth.tech@gmail.com>
2025-03-14 09:40:52 -0400

    Adding another file to showcase Python's ability to take inputs from user

---
+++ Python/userInput.py
@@ -0,0 +1,10 @@
+# Simple program to get the user input their name
+#Input fuction basically takes a user's input, lets put it in a variable so we can print it later
+name = input("Hello, I'm Python, what's your name?")
+
+#print user's input stored in varibvale name
+print("Hello, ",name)
+
+
+
+

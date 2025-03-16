30cd9b3b3feb156de5c1f9195fca85c4d13a5242
vyasparthm <vyasparth.tech@gmail.com>
2025-03-16 17:33:30 -0400

    Adding a simple calculator with  conversion to integer on user's input

---
+++ Python/Numbers.py
@@ -0,0 +1,13 @@
+"""
+This code file will have details about all it items you can do with numbers in python
+"""
+
+x = int(input("Add first value:"))
+y = int(input("Add second value:"))
+
+# z = int(x)+ int(y) #Convert using int otherwise the values are taken as string
+
+print(f"x + y =", x + y)
+
+
+

c48bac0341607972745a8a9a8a99eadef8a33438
vyasparthm <vyasparth.tech@gmail.com>
2025-11-06 21:42:29 -0500

    Added requirements

--- Python/Problem Sets/Problem Set 4/professor.py
+++ Python/Problem Sets/Problem Set 4/professor.py
@@ -1,3 +1,15 @@
+'''
+In a file called professor.py, implement a program that:
+
+Prompts the user for a level, n. If the user does not input 1, 2, or 3, the program should prompt again.
+Randomly generates ten (10) math problems formatted as X + Y = , wherein each of X and Y is a non-negative integer with n digits. No need to support operations other than addition (+).
+Note: The order in which you generate x and y matters. Your program should generate random numbers in x, y pairs to simulate generating one math question at a time (e.g., x0 with y0, x1 with y1, and so on).
+
+Prompts the user to solve each of those problems. If an answer is not correct (or not even a number), the program should output EEE and prompt the user again, allowing the user up to three tries in total for that problem. If the user has still not answered correctly after three tries, the program should output the correct answer.
+The program should ultimately output the user's score: the number of correct answers out of 10.
+Structure your program as follows, wherein get_level prompts (and, if need be, re-prompts) the user for a level and returns 1, 2, or 3, and generate_integer returns a single randomly generated non-negative integer with level digits or raises a ValueError if level is not 1, 2, or 3:
+'''
+
 import random
 
 def main():
